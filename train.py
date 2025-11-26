4c5b40b7dcb4be926811ab78204e11ad843951e0
Manursp0022 <s346705@studenti.polito.it>
2025-11-26 20:03:58 +0100

    training changes

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import argparse
 import os 
 import wandb
-from ultralytics import YOLO
+from ultralytics import YOLO, settings
 from utils.yolo_utils import create_yolo_labels, create_yolo_config
 
 def train():
@@ -12,17 +12,15 @@ def train():
     parser.add_argument('--model', type=str, default='yolov8n.pt')
     args = parser.parse_args()
 
+    settings.update({"wandb": True})
+
     print(f"___Data Preparation ___")
     create_yolo_labels(args.dataset_root)
     
     config_path = create_yolo_config(args.dataset_root)
-
-    save_dir = os.path.join(args.dataset_root, 'training_results')
-
+    
     print(f"___Starting training___")
     model = YOLO(args.model)
-
-
 
     model.train(
         data=config_path,
@@ -30,7 +28,7 @@ def train():
         patience=30,
         batch=args.batch,
         imgsz=640,
-        project=save_dir,      
+        project="linemod-detection",  
         name=f"YOLO_{args.model}_ep{args.epochs}",
         val=True,
         save=True,
@@ -38,7 +36,6 @@ def train():
         pretrained=True,
         optimizer='auto',
         verbose=True,
-        device=0
     )
 
 if __name__ == "__main__":
